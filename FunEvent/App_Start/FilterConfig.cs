4acd7eb942dd6057773a754d3b76a66bd5af84a0
dkumar <dkumar@osii.com>
2021-11-26 11:32:43 +0530

    Add project files.

---
+++ FunEvent/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FunEvent
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ FunEvent/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FunEvent.MvcApplication" Language="C#" %>
